e7c82a685cd51d81514b8cc210d4b92e5de38dc3
CerealJim <jim.haz34@gmail.com>
2023-10-23 15:13:50 -0400

    anagram & array chunking

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let objA = buildCharMap(stringA);
+  let objB = buildCharMap(stringB);
+
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  for (let property in objA) {
+    if (objA[property] !== objB[property]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//helper function
+const buildCharMap = (string) => {
+  const charMap = {};
+
+  for (let char of string.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+};
 
 module.exports = anagrams;
